a3cbf3c0ebe1b85ef583c7f31dc5d0b79350ae48
Ashwin <hotice@ashwin.com>
2014-07-18 11:45:54 +0530

    fixed to run

--- docs/models.py
+++ docs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Category(models.Model):
@@ -6,21 +7,23 @@ class Category(models.Model):
 	parent = models.ForeignKey('self', blank=True, null=True)
 	content = models.TextField()
 
+class Tag(models.Model):
+	name = models.CharField(max_length=20)
 
 class Doc(models.Model):
-	title
-	slug
-	category
-	tags
-	content
-	edited_by
-	edited_on
+	title = models.CharField(max_length=100)
+	slug = models.SlugField()
+	category = models.ForeignKey(Category)
+	tags = models.ManyToManyField(Tag)
+	content = models.TextField()
+	edited_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+	edited_on = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('slug', 'category')
+	class Meta:
+		unique_together = ('slug', 'category')
 
     
-class Resources(models.Model):
-	name
-	res_type
+# class Resources(models.Model):
+# 	name
+# 	res_type
 
--- microsite/wsgi.py
+++ microsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for micropyramidcom project.
+WSGI config for microsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "micropyramidcom.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microsite.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
