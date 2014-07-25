3983c049e174b55311904a84239ec81606dc8a6d
Ashwin <ashwin@micropyramid.com>
2014-07-26 02:42:14 +0530

    fixed auth

--- microauth/views.py
+++ microauth/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.context_processors import csrf
+from django.contrib.auth import login, authenticate, logout
+import json
 
-# Create your views here.
+
+@csrf_protect
 def index(request):
 	if request.user.is_authenticated():
 		return render_to_response('admin/index.html')
+	if request.method=="POST":
+		user = authenticate(username=request.POST.get('email'), password=request.POST.get('password'))
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				data = {'error':False}
+			else:
+				data = {'error':True,'message':"The password is valid, but the account has been disabled!"}
+		else:
+			data = {'error':True,'message':"The username and password were incorrect."}
+		return HttpResponse(json.dumps(data))
 	else:
-		return render_to_response('admin/login.html')
+    	data = {}
+    	data.update(csrf(request))
+    	return render_to_response('site/login.html',data)
 
 
 def out(request):
+	if not request.user.is_authenticated():
+		return HttpResponse('')
+
+	logout(request)
 	return HttpResponseRedirect('/')
