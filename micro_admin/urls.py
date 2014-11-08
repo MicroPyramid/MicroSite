8b74e2812d757837d157fbaae0f8bfb7d569a70f
Ashwin <ashwin@micropyramid.com>
2014-11-08 12:33:50 +0530

    "started user management"

--- micro_admin/urls.py
+++ micro_admin/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('micro_admin',
 
     url(r'^$', 'views.index', name='index'),
-    url(r'^user/change-password/$', 'views.change_password', name='change_password'),
     url(r'^contacts/$', 'views.contacts', name='contacts'),
     url(r'^jobs/$', 'views.jobs', name='jobs'),
-    url(r'^users/$', 'views.users', name='users'),
     url(r'^out/$', 'views.out', name='out'),
+
+    url(r'^users/$', 'users.users', name='users'),
+    url(r'^users/new$', 'users.new_user', name='new_user'),
+    url(r'^user/change-password/$', 'users.change_password', name='change_password'),
 )
