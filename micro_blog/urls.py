844216b29c9c5097bd063b709529e99d7b3b78d4
nikhila <nikhila@micropyramid.com>
2014-12-29 15:37:56 +0530

    blog sharing and comments

--- micro_blog/urls.py
+++ micro_blog/urls.py
@@ -9,26 +9,19 @@ urlpatterns = patterns('micro_blog.views',
     url(r'^admin/list/$', 'admin_post_list', name='admin_post_list'),
     url(r'^admin/new/$', 'admin_new_post', name='admin_new_post'),
     url(r'^edit/(?P<blog_slug>[-\w]+)/$', 'edit_blog_post', name='edit_post'),
-
     url(r'^category-list/$', 'admin_category_list', name='admin_category_list'),
     url(r'^new-category/$', 'new_blog_category', name='new_blog_category'),
-
+    url(r'^blog-comments/$', 'blog_comments', name='blog_comments'),
+    url(r'^delete-comments/(?P<comment_id>[-\w]+)/$', 'delete_blog_comments', name='delete_blog_comments'),
     url(r'^category/(?P<slug>[-\w]+)/$','blog_category'),
     url(r'^tag/(?P<slug>[-\w]+)/$','blog_tag'),
-
     url(r'^(?P<slug>[-\w]+)/$','blog_article'),
     url(r'^(?P<slug>[-\w]+)/add-comment/$','add_blog_comment'),
-
-    
-
     url(r'^(?P<year>\w{0,})/(?P<month>\w{0,})/$','archive_posts'),
-
     url(r'^change/featured-state/(?P<blog_slug>[-\w]+)/$', 'change_featured_state', name='change_featured_state'),
     url(r'^delete/blog-post/(?P<blog_slug>[-\w]+)/$', 'delete_post', name='delete_post'),
-
     url(r'^edit-category/(?P<category_slug>[-\w]+)/$', 'edit_category', name='edit_blog_category'),
     url(r'^delete-category/(?P<category_slug>[-\w]+)/$', 'delete_category', name='delete_blog_category'),
-
     url(r"^ajax/photos/upload/$", "upload_photos",name = "upload_photos"),
     url(r"^ajax/photos/recent/$", "recent_photos",name = "recent_photos"),
 
