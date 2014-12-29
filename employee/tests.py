d60b755b00a56883e4b1a83052ac1dca33795905
ravi <ravi@micropyramid.com>
2014-12-29 13:09:05 +0530

    travis.yml file added to checking

--- employee/tests.py
+++ employee/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+
+class TestBasic(unittest.TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
