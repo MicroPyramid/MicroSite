e1c3038847174c35999ddfee72917620d61d9910
nikhila <nikhila@micropyramid.com>
2015-02-18 17:26:47 +0530

    testcases changed

--- microsite/settings.py
+++ microsite/settings.py
@@ -65,8 +65,12 @@ BROKER_URL = 'redis://localhost:6379/0'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MICROSITEDBNAME'),
+        'USER': os.getenv('MICROSITEDBUSER'),
+        'PASSWORD': os.getenv('MICROSITEDBPASS'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
