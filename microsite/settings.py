9360e70e358b522abd6c4c3f492ed39beaa8382a
ravigadila <ravi@micropyramid.com>
2015-02-20 12:41:36 +0530

    chnaged db to mysql to run test cases

--- microsite/settings.py
+++ microsite/settings.py
@@ -65,8 +65,10 @@ BROKER_URL = 'redis://localhost:6379/0'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'micro',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
