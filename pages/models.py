f7d26bcfb7279edd07e33c9a7178b6df66f0fae8
nikhila05 <nikhila@micropyramid.com>
2015-02-26 19:24:03 +0530

    fixing landscape issues

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-
+from django.core.exceptions import ObjectDoesNotExist
 
 class Page(models.Model):
 	title = models.CharField(max_length=100)
