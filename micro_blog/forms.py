cf78c031f18e00372e9e85b8245e070a57a34444
Shirisha Gaddi <sirisha.gaddi@micropyramid.com>
2016-03-03 14:51:08 +0530

    Removed ajax form script from blog-new template and removed commented code from forms

--- micro_blog/forms.py
+++ micro_blog/forms.py
@@ -5,25 +5,10 @@ from django.db.models import Q
 
 class BlogpostForm(forms.ModelForm):
     meta_description = forms.CharField(max_length=500, required=False)
-    # slug = forms.CharField(max_length=500, required=False)
 
     class Meta:
         model = Post
         exclude = ('tags', 'user', 'meta_description', 'published_on', 'old_slugs')
-
-    # def __init__(self, *args, **kwargs):
-    #     self.user = kwargs.pop('user', "")
-    #     super(BlogpostForm, self).__init__(*args, **kwargs)
-    #     if self.data['is_superuser'] == 'True':
-    #         self.fields['slug'].required = True
-
-    # def clean_slug(self):
-    #     if self.data['is_superuser'] == 'True':
-    #         blog_posts = Post.objects.filter(Q(slug=self.data.get('slug')) | Q(old_slugs__icontains=self.data.get('slug'))).exclude(id=self.instance.id)
-    #         if blog_posts:
-    #             raise forms.ValidationError('Blog Post with this slug already exists')
-    #         else:
-    #             return self.data['slug']
 
 
 class BlogCategoryForm(forms.ModelForm):
