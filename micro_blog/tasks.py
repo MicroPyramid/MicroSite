0765fca891dbb1b5923578a8d48d2aae0b353b7e
Ashwin <ashwin@micropyramid.com>
2016-03-23 12:08:58 +0530

    enhanced microblog tasks

--- micro_blog/tasks.py
+++ micro_blog/tasks.py
@@ -1,7 +1,6 @@
 from celery.decorators import task
 import sendgrid
 from django.conf import settings
-from micro_admin.models import User
 from micro_blog.models import Subscribers, Post
 from .send_grid import *
 import datetime
@@ -11,7 +10,8 @@ import datetime
 def create_contact_in_category(category_name, email_address):
     '''
     Checks whether the category exists on not if it does then it will create a contact and save in
-    database under sendgrid_user_id field and then add contact to list. If not create a category list then contact then adds contact to the new category
+    database under sendgrid_user_id field and then add contact to list. If not create a category list
+    then contact then adds contact to the new category
     list which eliminates the duplicates, also if any contact or list already exists then it return
     the object which avoids creating duplicates.
 
@@ -25,13 +25,15 @@ def create_contact_in_category(category_name, email_address):
     contact_lists = get_contact_lists()
     if category_name in contact_lists:
         contact_id = create_contact(email_address)
-        CONTACTS_ENDPOINT = "https://api.sendgrid.com/v3/contactdb/lists/" + contact_lists[category_name]+"/" + "recipients/"+contact_id+""
+        CONTACTS_ENDPOINT = "https://api.sendgrid.com/v3/contactdb/lists/" + contact_lists[category_name] + \
+            "/" + "recipients/" + contact_id
         response = requests.post(CONTACTS_ENDPOINT, headers=headers)
         print response.status_code
     else:
         contact_list_id = create_contact_list(category_name)
         contact_id = create_contact(email_address)
-        CONTACTS_ENDPOINT = "https://api.sendgrid.com/v3/contactdb/" + "lists/{0}/recipients/{1}".format(contact_list_id, contact_id)
+        CONTACTS_ENDPOINT = "https://api.sendgrid.com/v3/contactdb/" + \
+            "lists/{0}/recipients/{1}".format(contact_list_id, contact_id)
         response = requests.post(CONTACTS_ENDPOINT, headers=headers)
         print response.status_code
 
@@ -50,8 +52,10 @@ def sending_mail_to_subscribers():
 
             message_reply = 'Hello ' + str(subscriber.email) + ',\n\n'
 
-            message_reply = '<p>New blog post has been created by ' + str(blog_post.author) + ' with the name ' + str(blog_post.title) + ' in the category '
-            message_reply += str(blog_post.category.name) + '.</p>' + '<p>Please <a href="' + blog_url + '">click here</a> to view the blog post in the site.</p>'
+            message_reply = '<p>New blog post has been created by ' + str(blog_post.author) + \
+                ' with the name ' + str(blog_post.title) + ' in the category '
+            message_reply += str(blog_post.category.name) + '.</p>' + '<p>Please <a href="' + \
+                blog_url + '">click here</a> to view the blog post in the site.</p>'
 
             message_reply = message_reply + 'Regards<br>'
             message_reply = message_reply + 'The MicroPyramid Team.<br>'
