c6df76f7816330750065e42f44645277ee63cd4e
Shirisha Gaddi <sirisha.gaddi@micropyramid.com>
2016-04-05 11:51:54 +0530

    Updated sendgrid file

--- micro_blog/send_grid.py
+++ micro_blog/send_grid.py
@@ -1,9 +1,7 @@
 import requests
-import base64
 from django.conf import settings
 
-authorization_key = "Basic " + base64.encodestring(''+settings.SG_USER+':'+settings.SG_PWD+'').replace('\n', '')
-headers = {'Authorization': authorization_key}
+headers = {'Authorization': settings.SG_AUTHORIZATION}
 
 
 def get_contact_lists():
@@ -20,12 +18,14 @@ def get_contact_lists():
 def create_contact(email_address):
     CONTACTS_ENDPOINT = "https://api.sendgrid.com/v3/contactdb/recipients"
     payload = [{"email": email_address}]
-    response = requests.post(CONTACTS_ENDPOINT, headers=headers, json=payload).json()
+    response = requests.post(
+        CONTACTS_ENDPOINT, headers=headers, json=payload).json()
     return response["persisted_recipients"][0]
 
 
 def create_contact_list(category_name):
     CONTACTS_ENDPOINT = "https://api.sendgrid.com/v3/contactdb/lists"
     payload = {"name": category_name}
-    response = requests.post(CONTACTS_ENDPOINT, headers=headers, json=payload).json()
+    response = requests.post(
+        CONTACTS_ENDPOINT, headers=headers, json=payload).json()
     return response["id"]
